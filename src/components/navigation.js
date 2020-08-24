d1f1fd7b5451d43f50d3dbda45a59e77cdf78b54
ademata <aldemata1@gmail.com>
2020-08-24 19:32:16 -0400

    added user authentication

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,20 +1,31 @@
 import React from "react"
 import {Link} from "gatsby"
 import ThemeChanger from "../components/themeChanger"
+import {useAuth} from 'react-use-auth'
 
-export default (props) => (
-  <nav className="navigation"> 
-    <Link to="/events">Events</Link>
-    <Link to="/jobs">Jobs</Link>
-    <Link to="/food">Food</Link>
-    <Link to="/housing">Housing</Link>
-    <Link to="/immigration">Immigration</Link>
-    <Link to="/buysell">Buy/Sell</Link>
-    <Link to="/movies">Movies</Link>
-    <Link to="/exchange">Exchange Rate</Link>
-    <Link to="/balikbayan">Balikbayan Box</Link>
-    <Link to="/contact">Contact</Link>
-    <ThemeChanger/>
-  </nav>
-  
-)
+
+//export default (props) => (
+
+const Navigation = () => {
+  const { isAuthenticated, login, logout } = useAuth()
+
+  return(
+    <nav className="navigation"> 
+      <Link to="/events">Events</Link>
+      <Link to="/jobs">Jobs</Link>
+      <Link to="/food">Food</Link>
+      <Link to="/housing">Housing</Link>
+      <Link to="/immigration">Immigration</Link>
+      <Link to="/buysell">Buy/Sell</Link>
+      <Link to="/movies">Movies</Link>
+      <Link to="/exchange">Exchange Rate</Link>
+      <Link to="/balikbayan">Balikbayan Box</Link>
+      <Link to="/contact">Contact</Link>
+      <button onClick={() => login()}>Login</button>
+      <button onClick={() => logout()}>Logout</button>
+      <ThemeChanger/>  
+    </nav> 
+  )
+}
+
+export default Navigation
