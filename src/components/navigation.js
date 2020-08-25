a3e942cbd36fe4ac5ca6753fefe30fea6d2aadce
ademata <aldemata1@gmail.com>
2020-08-25 18:04:46 -0400

    working code 08/25/20

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,13 +1,13 @@
 import React from "react"
 import {Link} from "gatsby"
 import ThemeChanger from "../components/themeChanger"
-import {useAuth} from 'react-use-auth'
+// import {useAuth} from 'react-use-auth'
 
 
 //export default (props) => (
 
 const Navigation = () => {
-  const { isAuthenticated, login, logout } = useAuth()
+  // const { isAuthenticated, login, logout } = useAuth()
 
   return(
     <nav className="navigation"> 
--- src/pages/auth0_callback.js
+++ src/pages/auth0_callback.js
@@ -1,27 +1,27 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
  
-import { useAuth } from "react-use-auth";
-// import Layout from "../components/layout";
+// import { useAuth } from "react-use-auth";
+// // import Layout from "../components/layout";
  
-const Auth0CallbackPage = () => {
-    const { handleAuthentication } = useAuth();
-    // useEffect(() => {
-    //     handleAuthentication();
-    // }, []);
+// const Auth0CallbackPage = () => {
+//     const { handleAuthentication } = useAuth();
+//     useEffect(() => {
+//         handleAuthentication();
+//     }, []);
 
-    useEffect(() => {
-        handleAuthentication();
-    }, []);
+//     useEffect(() => {
+//         handleAuthentication();
+//     }, []);
  
-    return (
-        <p>Loading...</p>
-    //     <Layout>
-    //         <h1>
-    //             This is the auth callback page, you should be redirected
-    //             immediately.
-    //         </h1>
-    //     </Layout>
-    );
-};
+//     return (
+//         <p>Loading...</p>
+//         <Layout>
+//             <h1>
+//                 This is the auth callback page, you should be redirected
+//                 immediately.
+//             </h1>
+//         </Layout>
+//     );
+// };
  
-export default Auth0CallbackPage;
+// export default Auth0CallbackPage;
